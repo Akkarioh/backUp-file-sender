e24b184f124d0c5d336d2348cc19b0cfdbbb1f79
Jose Regalado <jowav467@hotmail.com>
2025-07-26 14:22:49 -0600

    updated script to take arguments with quotes

--- backup_script.sh
+++ backup_script.sh
@@ -13,7 +13,7 @@ fi
 script_dir=$(dirname "$0") 
 
 # env file to store enviroment variables for the script and crontab to use
-source $script_dir/backup_config.env
+source $script_dir/backup.env
 
 # capture the current date, and store it in the format yyy-mm-dd
 current_date=$(date +%Y-%m-%d)
@@ -26,11 +26,11 @@ rsync_transfer="-av --remove-source-files"
 
 
 # commands rsync to transfer the specific files to back up and send all output to a log. 
-# the log stores in the same directory where the files are coming from
-$(which rsync) $rsync_options $1 $PI_USER@$PI_HOST:"$PI_PATH"/BackUp-Morgoth >> $script_dir/backup_$current_date.log
+# the log stores in the tmp directory until they are transfer to the target directory
+$(which rsync) $rsync_options "$1" $PI_USER@$PI_HOST:"$PI_PATH"/BackUp-Morgoth >> /tmp/backup_$current_date.log
 
 # then transfer that log file to the pi server as well. 
-$(which rsync) $rsync_transfer $script_dir/backup_$current_date.log $PI_USER@$PI_HOST:"$PI_PATH"/BackUp-Morgoth/backup_$current_date.log 
+$(which rsync) $rsync_transfer /tmp/backup_$current_date.log $PI_USER@$PI_HOST:"$PI_PATH"/BackUp-Morgoth/backup_$current_date.log 
 
 
 echo "Backup completed successfully on $current_date"
